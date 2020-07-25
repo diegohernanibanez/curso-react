270af25fe1bf25b45f39ab2003fdf9a90bced779
Diego Ibañez <diego.hernan.ibanez@gmail.com>
2020-07-25 19:34:05 -0300

    PrimeraApp test con enzyme

--- 03-counter-app/src/tests/PrimeraApp.test.js
+++ 03-counter-app/src/tests/PrimeraApp.test.js
@@ -1,14 +1,24 @@
 import React from 'react';
-import {render} from "@testing-library/react";
+// import {render} from "@testing-library/react";
 import PrimeraApp from "../PrimeraApp";
+import {shallow} from 'enzyme';
 
 describe('Pruena en PrimeraApp', () => {
-    test('debe mostrar "Hola Mundo"', () => {
-        const saludo = 'Hola Mundo';
+    // test('debe mostrar "Hola Mundo"', () => {
+    //     const saludo = 'Hola Mundo';
 
-        const {getByText} = render(<PrimeraApp saludo = {saludo} />);
+    //     const {getByText} = render(<PrimeraApp saludo = {saludo} />);
         
-        expect(getByText(saludo)).toBeInTheDocument();
+    //     expect(getByText(saludo)).toBeInTheDocument();
+    // })
+    
+    test('debe mostrar PrimeraApp correctamente', () => {
+        const saludo = 'Hola Mundo';
+        const wrapper = shallow(<PrimeraApp saludo = {saludo} />);
+
+
+        expect(wrapper).toMatchSnapshot();
     })
     
+
 })
