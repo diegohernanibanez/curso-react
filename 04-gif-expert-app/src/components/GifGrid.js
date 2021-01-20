52df193a3cced0bed688fe6dc88b36b386f1c257
Diego Hernan <diego.hernan.ibanez@gmail.com>
2021-01-20 00:09:15 -0300

    Primer hook con funcionalidad de spinner

--- 04-gif-expert-app/src/components/GifGrid.js
+++ 04-gif-expert-app/src/components/GifGrid.js
@@ -1,23 +1,27 @@
-import React, { useState, useEffect } from 'react'
-import { getGifs } from '../helpers/getGifs';
-import { GifGridItem } from './GifGridItem';
+import React from 'react'
+import { useFetchGifs } from '../hooks/useFetchGifs'
+// import { getGifs } from '../helpers/getGifs';
+// import { GifGridItem } from './GifGridItem';
 
 export const GifGrid = ({category}) => {
 
-    const [images, setImages] = useState([]);
+    // const [images, setImages] = useState([]);
 
-    useEffect(() => {
-        getGifs(category)
-        .then( setImages)
-    }, [category]);
+    // useEffect(() => {
+    //     getGifs(category)
+    //     .then( setImages)
+    // }, [category]);
 
-    
-    
+    const {loading} = useFetchGifs();
+    console.log(loading);
     
     return (
         <>
             <h3>{category}</h3>
-            <div className='card-grid'>
+            
+            {loading ? 'Cargando...' : 'Fin de carga'}
+
+            {/* <div className='card-grid'>
                     {
                         images.map( img => (
                             <GifGridItem 
@@ -26,7 +30,7 @@ export const GifGrid = ({category}) => {
                             />
                         ))
                     }
-            </div>
+            </div> */}
         </>
     )
 }
