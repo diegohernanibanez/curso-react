1d6cf2dfc9ca97496e02b3e45c83e98d4f2e980a
Diego Hernan <diego.hernan.ibanez@gmail.com>
2021-01-20 00:25:32 -0300

    Se termina el primer custom hook con carga de imagenes

--- 04-gif-expert-app/src/components/GifGrid.js
+++ 04-gif-expert-app/src/components/GifGrid.js
@@ -1,27 +1,18 @@
 import React from 'react'
 import { useFetchGifs } from '../hooks/useFetchGifs'
-// import { getGifs } from '../helpers/getGifs';
-// import { GifGridItem } from './GifGridItem';
+import { GifGridItem } from './GifGridItem';
 
 export const GifGrid = ({category}) => {
 
-    // const [images, setImages] = useState([]);
-
-    // useEffect(() => {
-    //     getGifs(category)
-    //     .then( setImages)
-    // }, [category]);
-
-    const {loading} = useFetchGifs();
-    console.log(loading);
+    const {data:images, loading} = useFetchGifs(category);
     
     return (
         <>
             <h3>{category}</h3>
-            
-            {loading ? 'Cargando...' : 'Fin de carga'}
 
-            {/* <div className='card-grid'>
+            {loading && <p>Cargando</p>}
+
+            <div className='card-grid'>
                     {
                         images.map( img => (
                             <GifGridItem 
@@ -30,7 +21,7 @@ export const GifGrid = ({category}) => {
                             />
                         ))
                     }
-            </div> */}
+            </div>
         </>
     )
 }
